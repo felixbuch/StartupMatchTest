8b53b483097347371123b654a15b43192435564a
friedrich8968 <f.albrecht@stud.hs-wismar.de>
2023-02-06 16:22:25 +0100

    implemented check on valid name

---
+++ src/email-validation.js
@@ -0,0 +1,27 @@
+//in dieser datei könnten reguläre ausdrücke für name und irgendwann gegebenfalls passwörter ergänzt werden
+const inputs = document.querySelectorAll('input');
+
+// regex patterns
+const patterns = {
+    name: /^[a-z\d]{5,12}$/i,
+    email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/
+};
+
+// validation function
+function validate(field, regex){
+
+    if(regex.test(field.value)){
+        field.className = 'valid';
+    } else {
+        field.className = 'invalid';
+    }
+
+}
+
+// attach keyup events to inputs
+inputs.forEach((input) => {
+    input.addEventListener('keyup', (e) => {
+        // console.log(patterns[e.target.attributes.name.value]);
+        validate(e.target, patterns[e.target.attributes.name.value]);
+    });
+});
